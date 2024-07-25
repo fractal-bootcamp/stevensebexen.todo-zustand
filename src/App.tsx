00bde7af2f746031d08ff7473285d398779002cd
Steven <ssebexen@gmail.com>
2024-07-25 15:23:55 -0400

    Error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import useAppViewStore from './stores/AppViewStore'
+import ErrorView from './views/ErrorView';
 import views from './views/views';
 
 function App() {
   const {appView} = useAppViewStore();
-  const ViewComponent = views[appView];
+  const ViewComponent = views[appView] ?? ErrorView;
   return (
     <>
       <NavBar />
